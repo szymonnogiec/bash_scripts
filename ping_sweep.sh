22faffe4fee7cd64d48aef3138b24028aa098b5e
szymon.nogiec@gmail.com <szymon.nogiec@gmail.com>
2018-09-23 18:16:25 +0200

    Added simple ping sweep script

---
+++ ping_sweep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $1 == "" ]
+then
+echo "Usage: ./ping_sweep.sh [network]
+echo "Example: ./ping_sweep.sh 192.168.20
+else
+for x in {1..254}
+do
+	ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f4 | sed 's/.$//'
+done
+fi
+
+
